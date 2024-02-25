132d6cc9ef2a8c3e5b91b4c7a0d24c7b56f12589
Subodh Rana <40374844+Subodhrana390@users.noreply.github.com>
2024-02-26 00:20:47 +0530

    add adsense script

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,10 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <UserMemberContext.Provider value={{ isMember, setIsMember }}>
         <html lang="en">
+          <head>
+              <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7199918361492779"
+               crossorigin="anonymous"></script>
+          </head>
           <body className={inter.className}>
             {children}
             <Toaster />
